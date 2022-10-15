13b8808ef9000d7ab1ecdf2be75fcdfda8b2ea2e
Evans Roy <evansroysir@gmail.com>
2022-10-16 01:58:23 +0300

    Added approve and Cancel button in the show appointment blade

--- resources/views/admin/showappointment.blade.php
+++ resources/views/admin/showappointment.blade.php
@@ -16,10 +16,10 @@
 
             <div class="container-fluid page-body-wrapper" >
                 <div align = "center" style="padding: 100px">
-                    <table class="table table-striped">
+                    <table class="table table-striped table-dark">
                         <thead>
                           <tr>
-                           
+
                             <th scope="col">Customer Name</th>
                             <th scope="col">Email</th>
                             <th scope="col">Phone</th>
@@ -27,6 +27,8 @@
                             <th scope="col">Date</th>
                             <th scope="col">Message</th>
                             <th scope="col">Status</th>
+                            <th scope="col">Approve</th>
+                            <th scope="col">Cancel</th>
                           </tr>
                         </thead>
                         <tbody>
@@ -40,6 +42,14 @@
                                     <td>{{$appoint->date}}</td>
                                     <td>{{$appoint->message}}</td>
                                     <td>{{$appoint->status}}</td>
+
+                                    <td>
+                                        <a class="btn btn-success" href="">Approve</a>
+                                    </td>
+
+                                    <td>
+                                        <a class="btn btn-danger" href="">Cancel</a>
+                                    </td>
                                 </tr>
                             @endforeach
 
