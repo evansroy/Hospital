643ad39d74050439189e88b3c65022f57889b314
Evans Roy <evansroysir@gmail.com>
2022-11-03 11:32:53 +0300

    Made show appointmen and show doctor table responsive

--- resources/views/admin/showappointment.blade.php
+++ resources/views/admin/showappointment.blade.php
@@ -14,47 +14,52 @@
 
             @include('admin.navbar')
 
-            <div class="container-fluid page-body-wrapper" >
-                <div align = "center" style="padding: 100px">
-                    <table class="table table-striped table-dark">
-                        <thead>
-                          <tr>
+            <div class="main-panel" >
+                <div class="content-wrapper">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-dark">
+                            <thead>
+                              <tr>
 
-                            <th scope="col">Customer Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Phone</th>
-                            <th scope="col">Doctor Name</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Message</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">Approve</th>
-                            <th scope="col">Cancel</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($data as $appoint)
-                                <tr>
+                                <th scope="col">Customer Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Doctor Name</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Message</th>
+                                <th scope="col">Status</th>
+                                {{-- <th scope="col">Approve</th>
+                                <th scope="col">Cancel</th> --}}
+                                <th scope="col">Actions</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($data as $appoint)
+                                    <tr>
 
-                                    <td>{{$appoint->name}}</td>
-                                    <td>{{$appoint->email}}</td>
-                                    <td>{{$appoint->phone}}</td>
-                                    <td>{{$appoint->doctor}}</td>
-                                    <td>{{$appoint->date}}</td>
-                                    <td>{{$appoint->message}}</td>
-                                    <td>{{$appoint->status}}</td>
+                                        <td>{{$appoint->name}}</td>
+                                        <td>{{$appoint->email}}</td>
+                                        <td>{{$appoint->phone}}</td>
+                                        <td>{{$appoint->doctor}}</td>
+                                        <td>{{$appoint->date}}</td>
+                                        <td>{{$appoint->message}}</td>
+                                        <td>{{$appoint->status}}</td>
 
-                                    <td>
-                                        <a class="btn btn-success" href="{{url('approved',$appoint->id)}}">Approve</a>
-                                    </td>
+                                        <td>
+                                            <a class="btn btn-success" href="{{url('approved',$appoint->id)}}">Approve</a>
+                                            <a class="btn btn-danger" href="{{url('cancelled',$appoint->id)}}">Cancel</a>
+                                            <a href="" class="btn btn-primary" href="{{url('')}}">Send Email</a>
 
-                                    <td>
-                                        <a class="btn btn-danger" href="{{url('cancelled',$appoint->id)}}">Cancel</a>
-                                    </td>
-                                </tr>
-                            @endforeach
+                                        </td>
 
-                        </tbody>
-                      </table>
+
+                                    </tr>
+                                @endforeach
+
+                            </tbody>
+                          </table>
+
+                    </div>
                 </div>
             </div>
 
--- resources/views/admin/showdoctor.blade.php
+++ resources/views/admin/showdoctor.blade.php
@@ -14,46 +14,49 @@
 
             @include('admin.navbar')
 
-            <div class="container-fluid page-body-wrapper" >
-                <div align = "center" style="padding: 100px">
-                    <table class="table table-striped table-dark">
-                        <thead>
-                          <tr>
+            <div class="main-panel" >
+                <div class="content-wrapper">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-dark">
+                            <thead>
+                              <tr>
 
-                            <th scope="col">Doctor Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Phone</th>
-                            <th scope="col">Speciality</th>
-                            <th scope="col">Room No</th>
-                            <th scope="col">Profile Image</th>
-                            <th scope="col">Delete</th>
-                            <th scope="col">Update</th>
+                                <th scope="col">Doctor Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Speciality</th>
+                                <th scope="col">Room No</th>
+                                <th scope="col">Profile Image</th>
+                                <th scope="col">Delete</th>
+                                <th scope="col">Update</th>
 
-                          </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($data as $doctor)
-                                <tr>
+                              </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($data as $doctor)
+                                    <tr>
 
-                                    <td>{{$doctor->name}}</td>
-                                    <td>{{$doctor->email}}</td>
-                                    <td>{{$doctor->phone}}</td>
-                                    <td>{{$doctor->specialty}}</td>
-                                    <td>{{$doctor->room}}</td>
-                                    <td> <img src="doctorimage/{{$doctor->image}}" alt="Profile Image"></td>
-                                    <td>
-                                        <a class="btn btn-danger" href="{{url('delete_doctor',$doctor->id)}}" onclick="return confirm('Are you sure you want to Delete This Doctor ?')">Delete</a>
-                                    </td>
+                                        <td>{{$doctor->name}}</td>
+                                        <td>{{$doctor->email}}</td>
+                                        <td>{{$doctor->phone}}</td>
+                                        <td>{{$doctor->specialty}}</td>
+                                        <td>{{$doctor->room}}</td>
+                                        <td> <img src="doctorimage/{{$doctor->image}}" alt="Profile Image"></td>
+                                        <td>
+                                            <a class="btn btn-danger" href="{{url('delete_doctor',$doctor->id)}}" onclick="return confirm('Are you sure you want to Delete This Doctor ?')">Delete</a>
+                                        </td>
 
-                                    <td>
-                                        <a class="btn btn-primary" href="{{url('update_doctor',$doctor->id)}}">Update</a>
-                                    </td>
+                                        <td>
+                                            <a class="btn btn-primary" href="{{url('update_doctor',$doctor->id)}}">Update</a>
+                                        </td>
 
-                                </tr>
-                            @endforeach
+                                    </tr>
+                                @endforeach
 
-                        </tbody>
-                      </table>
+                            </tbody>
+                          </table>
+                    </div>
+
                 </div>
             </div>
 
